f65649ba032f77ebe256a41c7db71af3a57b9b89
Henrique Valcanaia <henriqueindalencio@gmail.com>
2016-09-15 13:00:28 -0300

    Initial Commit

---
+++ cthread/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cthread
+//
+//  Created by Henrique Valcanaia on 15/09/16.
+//  Copyright © 2016 Henrique Valcanaia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
