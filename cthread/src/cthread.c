0812f9a9a872400a0303049bf3df3fd7ef2011df
Henrique Valcanaia <henriqueindalencio@gmail.com>
2016-09-27 14:13:44 -0300

    Alterado CamelCase para Snake case, ajustes para compilação.

--- cthread/src/cthread.c
+++ cthread/src/cthread.c
@@ -24,25 +24,25 @@ PFILA2 blocked;
 int threadId = 1;
 
 int schedule() {
-    TCB_t nextThread;
-    if (getNextThread(&nextThread) == 0) {
+    TCB_t next_thread;
+    if (get_next_thread(&next_thread) == 0) {
         return 0;
     } else {
         return -1;
     }
 }
 
-int getNextThread(TCB_t* nextThread) {
+int get_next_thread(TCB_t* next_thread) {
     int randomTicket = generateTicket();
-    if (getThreadClosestToTicket(randomTicket, nextThread) == 0) {
+    if (get_thread_closest_to_ticket(randomTicket, next_thread) == 0) {
         return  0;
     } else {
         return -1;
     }
 }
 
-int getThreadClosestToTicket() {
-    
+int get_thread_closest_to_ticket() {
+    return 1;
 }
 
 /*!
@@ -50,7 +50,7 @@ int getThreadClosestToTicket() {
  @discussion Utiliza a funcao Random2() da biblioteca support.o disponibilizada pelo professor
  @return Inteiro aleatorio de 1 byte
  */
-int generateTicket() {
+int generate_ticket() {
     int ticket = Random2() % 255;
     return ticket;
 }
@@ -59,7 +59,7 @@ int generateTicket() {
  @brief Gera um novo identificador de thread
  @discussion Este identificador é sequencial e não é reutilizado. Use com sabedoria.
  */
-int generateThreadId() {
+int generate_thread_id() {
     threadId++;
     return threadId;
 }
@@ -67,7 +67,7 @@ int generateThreadId() {
 /*!
  @brief Adiciona um TCB a uma fila
  */
-int addThreadToQueue(TCB_t* thread, PFILA2 queue) {
+int add_thread_to_queue(TCB_t* thread, PFILA2 queue) {
     int result = AppendFila2(queue, (void*) thread);
     return result;
 }
@@ -75,16 +75,16 @@ int addThreadToQueue(TCB_t* thread, PFILA2 queue) {
 /*!
  @brief Adiciona um TCB a fila de aptos
  */
-int addThreadToReadyQueue(TCB_t* thread) {
-    int result = addThreadToQueue(thread, ready);
+int add_thread_to_ready_queue(TCB_t* thread) {
+    int result = add_thread_to_queue(thread, ready);
     return result;
 }
 
 /*!
  @brief Adiciona um TCB a fila de bloqueados
  */
-int addThreadToBlockedQueue(TCB_t* thread) {
-    int result = addThreadToQueue(thread, blocked);
+int add_thread_to_blocked_queue(TCB_t* thread) {
+    int result = add_thread_to_queue(thread, blocked);
     return result;
 }
 
@@ -130,7 +130,7 @@ int ccreate (void *(*start)(void *), void *arg) {
     tcb->state = CREATION;
     tcb->ticket = generateTicket();
     if (getcontext(&tcb->context) == 0) {
-        if (addThreadToReadyQueue(tcb) == 0) {
+        if (add_thread_to_ready_queue(tcb) == 0) {
             return tcb->tid;
         } else {
             return CCREATE_ERROR;
@@ -141,19 +141,19 @@ int ccreate (void *(*start)(void *), void *arg) {
 }
 
 void create_main_context() {
-    char function_stack = char[SIGSTKSZ];
-    main_context = (ucontext_t*) malloc(sizeof(context));
+    char function_stack[SIGSTKSZ];
+    main_context = (ucontext_t*) malloc(sizeof(ucontext_t));
     getcontext(&main_context);
     main_context->uc_stack.ss_sp = function_stack;
     main_context->uc_stack.ss_size = sizeof(function_stack);
     main_context->uc_link = NULL;
     makecontext(main_context, NULL, 0);
 
-    TCB_t main = malloc(sizeof(TCB_t)); 
+    TCB_t main;
     main.tid = 0;
     main.state = THREAD_STATE_READY;
     main.ticket = generateTicket(); 
-    getcontext(&main->context);
+    getcontext(&(main.context));
     running_thread = &main;
 }
 
@@ -180,7 +180,7 @@ int cjoin(int tid) {
         TCB_t* running_thread = get_running_thread();
         assert("ver atribuicao abaixo struct TCB_T* com TCB_t*");
         join_list[tid] = running_thread;
-        addThreadToBlockedQueue(running_thread);
+        add_thread_to_blocked_queue(running_thread);
         
         return CJOIN_SUCCESS;
     }
