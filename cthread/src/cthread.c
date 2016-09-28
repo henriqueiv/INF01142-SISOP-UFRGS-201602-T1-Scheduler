d2f06b8d3686fd2a6362f6c91c6a818403f4f98b
Henrique Valcanaia <henriqueindalencio@gmail.com>
2016-09-28 00:39:10 -0300

    debug_print

--- cthread/src/cthread.c
+++ cthread/src/cthread.c
@@ -29,22 +29,22 @@ int thread_id = 1;
 
 void print_queue(FILA2 queue) {
     if (FirstFila2(&queue) != 0) {
-        DEBUG_PRINT("ERRO ou FILA VAZIA\n");
+        printf("ERRO ou FILA VAZIA\n");
         return;
     }
     
     TCB_t* currentTCB = (TCB_t*) malloc(sizeof(TCB_t));
     int i = 0;
     do {  
-        DEBUG_PRINT("%d\n", i);
+        printf("%d\n", i);
         currentTCB = GetAtIteratorFila2(&queue);
         if (currentTCB == NULL)
             break;
             
-        DEBUG_PRINT("pos(%d) tid(%d)\n", i, currentTCB->tid);
+        printf("pos(%d) tid(%d)\n", i, currentTCB->tid);
         i++;
     } while (NextFila2(&queue) == 0);
-    DEBUG_PRINT("fim\n");
+    printf("fim\n");
 }
 
 
@@ -133,14 +133,14 @@ int is_blocked(int tid) {
 // ================ SCHEDULE ================
 
 void schedule() {
-    DEBUG_PRINT("SCHEDULE\n");
+    printf("SCHEDULE\n");
     if (FirstFila2(&ready) != 0) {
-        DEBUG_PRINT("ERRO OU FILA VAZIA\n");
+        printf("ERRO OU FILA VAZIA\n");
         return;
     }
     
     TCB_t* next_thread = GetAtIteratorFila2(&ready);
-    DEBUG_PRINT("next_thread: tid(%d)\n", next_thread->tid);
+    printf("next_thread: tid(%d)\n", next_thread->tid);
     
     running_thread = next_thread;
     DeleteAtIteratorFila2(&ready);
@@ -210,7 +210,7 @@ int ccreate (void *(*start)(void *), void *arg) {
 }
 
 int cyield() {
-    DEBUG_PRINT("Running: %d\n", running_thread->tid);
+    printf("Running: %d\n", running_thread->tid);
     
     TCB_t* thread;
     thread = running_thread;
